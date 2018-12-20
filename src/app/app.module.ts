ebc6e2e252b8a30c5c2211e79f4162b9d4ab2225
bala2695 <balachandar26011995@gmail.com>
2018-12-20 15:55:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CustomerComponent } from './customer/customer.component';
+import { DeliveryOrderComponent } from './delivery-order/delivery-order.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule,Routes } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CustomerComponent,
+    DeliveryOrderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:"Customer",component:CustomerComponent},
+      {path:"Login",component:LoginComponent},
+      {path:"Delivery",component:DeliveryOrderComponent},
+      {path:"",component:LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
